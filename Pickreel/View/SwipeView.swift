4a16833ad106966b47b198503de3b1627631c182
justUzm <uzmel@icloud.com>
2024-03-31 14:51:11 +0300

    add review

---
+++ Pickreel/View/SwipeView.swift
@@ -0,0 +1,20 @@
+//
+//  SwipeView.swift
+//  Pickreel
+//
+//  Created by Усман Махмутхажиев on 31.03.2024.
+//
+
+import UIKit
+
+class SwipeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
